28687d63f3796ef8f37bee50e1f23b23ccc1453e
roselandroche <r.landroche@gmail.com>
2019-08-27 19:43:30 -0400

    Moved axios call into useEffect

--- src/APOD.js
+++ src/APOD.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function APOD () {
     const [spacePic, updateSpacePic] = useState()
 
-    axios
-        .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    useEffect(() => {
+        axios
+            .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
 
-        .then(result => updateSpacePic(`${result.data.hdurl}`))
+            .then(result => updateSpacePic(`${result.data.hdurl}`))
 
-        .catch(error => console.log(error))
+            .catch(error => console.log(error))
+    }, []);
     
     return <img src={spacePic} alt='The vast universe'></img>
 }
