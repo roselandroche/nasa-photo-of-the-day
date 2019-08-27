19a08ab5dcd987e79a7654f329f41cba13a8c5ff
roselandroche <r.landroche@gmail.com>
2019-08-27 19:33:13 -0400

    Created my image module, imported to App

---
+++ src/APOD.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function APOD () {
+    const [spacePic, updateSpacePic] = useState()
+
+    axios
+        .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+
+        .then(result => updateSpacePic(`${result.data.hdurl}`))
+
+        .catch(error => console.log(error))
+    
+    return <img src={spacePic} alt='The vast universe'></img>
+}
+
+export default APOD
--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
-import Header from './header'
+import Header from './Header'
+import APOD from './APOD'
 
 function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun!
       </p>
       <Header />
+      <APOD />
     </div>
   );
 }
