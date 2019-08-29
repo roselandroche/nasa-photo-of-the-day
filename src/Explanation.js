e4d6086844369d10d83a0bbeb326dc9ee32c9094
roselandroche <r.landroche@gmail.com>
2019-08-28 21:55:57 -0400

    Styled explanation

--- src/Explanation.js
+++ src/Explanation.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledExplanation = styled.p`
+    width: 90%;
+    padding: 10px;
+    text-align: left;
+    font-family: Courier New;
+`;
 
 function Explanation(props) {
-    return <p className='explanation'>{props.explanation}</p>
+    return <StyledExplanation className='explanation'>{props.explanation}</StyledExplanation>
 }
 
 export default Explanation
